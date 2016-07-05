32f0cf994363fda4a4ed1a811d81f14bd4ac3a47
Mezo Istvan <istvan.mez@ge.com>
2016-07-05 12:42:45 +0200

    initial mock userService

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
-angular.module("userServiceModule", [])
+angular.module("userServiceModule", ["authServiceModule"])
 
-.factory("userService", function () {
-    return {
+.factory("userService", function (authService, $q) {
+	
+	var fac = {};
+	
+	fac.write2Console = function() {
+		console.log("userService is up");
+	};
+	
+	fac.createUser = function(userName, password) {
+		var newUser = { 
+			"userName" : userName, 
+			"password" : password, 
+			"role" : "USER" 
+		};
+		
+		var deferred = $q.defer();
+		deferred.resolve(newUser);
+		
+		return deferred.promise;
+	};
+	
+	fac.changeUserPassword = function(newPassword, oldPassword) {
+		var userWithChangedPassword = { 
+			"userName" : "userName", 
+			"password" : null, 
+			"role" : null 
+		};
+		var deferred = $q.defer();
+		deferred.resolve(userWithChangedPassword);
+		
+		return deferred.promise;
+	};
+	
+	fac.setRole = function(username, role) {
+		var deferred = $q.defer();
+		deferred.resolve();
+		
+		return deferred.promise;
+	};
 
-    }
+	fac.getUser = function() {
+		var user = { 
+			"userName" : "userName", 
+			"password" : "password", 
+			"role" : "ADMIN" 
+		};
+		
+		var deferred = $q.defer();
+		deferred.resolve(user);
+		
+		return deferred.promise;	
+	};
+	
+	fac.getUsers = function() {
+		var users = [{ 
+			"userName" : "userName", 
+			"password" : "password", 
+			"role" : "ADMIN" 
+		}, {
+			"userName" : "userName2", 
+			"password" : "password2", 
+			"role" : "USER"	
+		}];
+		
+		var deferred = $q.defer();
+		deferred.resolve(users);
+		
+		return deferred.promise;	
+	};
+	
+    return fac;
 });
