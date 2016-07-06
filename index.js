83bf6b3c5cd22637ccb5360fcc836c191bb0d792
László Kiss <laszlo.kiss1@ge.com>
2016-07-06 16:23:30 +0200

    Implementation for user services. test.js was created. version in package.json was incremented.

--- index.js
+++ index.js
@@ -1,80 +1,156 @@
 angular.module("userServiceModule", ["authServiceModule"])
 
-.factory("userService", function (authService, $q) {
+.factory("userService", function ($httpWithProtection, $q) {
 	
 	var fac = {};
-	
-	fac.createUser = function(userName, password) {
+
+    /*
+    Parameter
+         userModel =
+         {
+            userName : userName,
+            password : password,
+            role : "ADMIN || USER"
+         }
+     */
+	fac.createUser = function(userModel) {
 		var deferred = $q.defer();
-//		if (!userName || !password) {
-			
-			var newUser = { 
-				"userName" : userName, 
-				"password" : password, 
-				"role" : "USER" 
-			};
-			deferred.resolve(newUser);
-			
-//		} else {
-//			
-//			var errorObj = {
-//				"message": "MOCK Missing data",
-//				"statusCode": 401,
-//			};
-//			deferred.reject(errorObj);
-//			
-//		}
-		return deferred.promise;
-	};
-	
-	fac.changeUserPassword = function(newPassword, oldPassword) {
-		var userWithChangedPassword = { 
-			"userName" : "userName", 
-			"password" : null, 
-			"role" : null 
+
+        //TODO: Validation
+
+		var request = {
+			url: "http://localhost:8080/users/create",
+            method: "POST",
+			data: userModel
 		};
-		var deferred = $q.defer();
-		deferred.resolve(userWithChangedPassword);
-		
-		return deferred.promise;
-	};
-	
-	fac.setRole = function(username, role) {
-		var deferred = $q.defer();
-		deferred.resolve();
-		
+		var http = $httpWithProtection(request).then(function(result){
+            deferred.resolve(result);
+        }, function(reason){
+            deferred.reject(reason);
+        });
+
 		return deferred.promise;
 	};
 
-	fac.getUser = function() {
-		var user = { 
-			"userName" : "userName", 
-			"password" : "password", 
-			"role" : "ADMIN" 
-		};
-		
+	/*
+	Parameter
+         passwordChangeModel =
+         {
+             oldPassword : "oldPassword",
+             newPassword : "newPassword"
+         }
+	 */
+	fac.changeUserPassword = function(passwordChangeModel) {
+
 		var deferred = $q.defer();
-		deferred.resolve(user);
-		
-		return deferred.promise;	
+        //TODO: Validation
+
+        var request = {
+            url: "http://localhost:8080/users/changepassword",
+            method: "PUT",
+            data: passwordChangeModel
+        };
+        var http = $httpWithProtection(request).then(function(result){
+            deferred.resolve(result);
+        }, function(reason){
+            deferred.reject(reason);
+        });
+
+		return deferred.promise;
 	};
-	
+
+    /*
+    Parameter
+        assignRoleModel =
+        {
+            userName : "userName",
+            role: "ADMIN || USER"
+        }
+     */
+	fac.setRole = function(assignRoleModel) {
+        var deferred = $q.defer();
+        //TODO: Validation
+
+        var request = {
+            url: "http://localhost:8080/users/assignrole",
+            method: "POST",
+            data: assignRoleModel
+        };
+        var http = $httpWithProtection(request).then(function(result){
+            deferred.resolve(result);
+        }, function(reason){
+            deferred.reject(reason);
+        });
+
+        return deferred.promise;
+	};
+
+    /*
+    Parameter
+         userModel =
+         {
+             userName : userName,
+             password : password,
+             role : "ADMIN || USER"
+         }
+
+    return
+        userModel =
+        {
+            userName : "userName",
+            password : "password",
+            role : "ADMIN || USER"
+        }
+     */
+	fac.getUser = function(userModel) {
+        var deferred = $q.defer();
+        //TODO: Validation
+
+        var request = {
+            url: "http://localhost:8080/users/" + userModel.userName,
+            method: "GET",
+        };
+        var http = $httpWithProtection(request).then(function(result){
+            deferred.resolve(result);
+        }, function(reason){
+            deferred.reject(reason);
+        });
+
+        return deferred.promise;
+	};
+
+    /*
+        return
+         [
+              {
+              "userName" : "userName",
+              "password" : "password",
+              "role" : "ADMIN"
+              },
+              {
+              "userName" : "userName2",
+              "password" : "password2",
+              "role" : "USER"
+              }
+         ]
+     */
 	fac.getUsers = function() {
-		var users = [{ 
-			"userName" : "userName", 
-			"password" : "password", 
-			"role" : "ADMIN" 
-		}, {
-			"userName" : "userName2", 
-			"password" : "password2", 
-			"role" : "USER"	
-		}];
-		
-		var deferred = $q.defer();
-		deferred.resolve(users);
-		
-		return deferred.promise;	
+
+        var deferred = $q.defer();
+        //TODO: Validation
+
+        var request = {
+            url: "http://localhost:8080/users",
+            method: "GET",
+        };
+        var http = $httpWithProtection(request).then(function(result){
+            deferred.resolve(result);
+        }, function(reason){
+            deferred.reject(reason);
+        });
+
+        return deferred.promise;
 	};
-	
+
     return fac;
 });
